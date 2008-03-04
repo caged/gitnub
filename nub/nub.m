53b383ee6d164ddfb958afed42b73914abf43b14
Justin Palmer <justin@activereload.net>
2008-03-03 21:06:56 -0800

    No need to terminate nub after launching the task, it automagically terminates

--- nub/nub.m
+++ nub/nub.m
@@ -9,7 +9,6 @@ int main (int argc, const char * argv[]) {
     NSTask *task = [[NSTask alloc] init];
     [task setLaunchPath:@"/Applications/GitNub.app/Contents/MacOS/GitNub"];
     [task launch];
-    [task terminate];
     
     [pool drain];
     return 0;
