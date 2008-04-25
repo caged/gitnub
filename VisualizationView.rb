8230ca3ae817a025a64a2aa2af1bf13cda4391a1
Justin Palmer <justin@activereload.net>
2008-04-25 14:02:08 -0700

    Some playing with the visualization.  Getting a clean head

--- VisualizationView.rb
+++ VisualizationView.rb
@@ -7,6 +7,8 @@
 #
 
 require 'osx/cocoa'
+require 'ostruct'
+require 'pp'
 
 class VisualizationView <  OSX::NSView
   ib_outlet :application_controller
@@ -26,20 +28,49 @@ class VisualizationView <  OSX::NSView
   end
   
   def drawRect(rect)
-    points = []
+    nodes = {}
     context = NSGraphicsContext.currentContext
     NSColor.darkGrayColor.set
-    NSRectFill(self.bounds)
+    NSRectFill(bounds)
     
     path = NSBezierPath.bezierPath
-    path.lineWidth = 2
+    path.lineWidth = 3
+
+    path.moveToPoint([100, 105])
+    path.appendBezierPathWithOvalInRect(NSMakeRect(100, 100, 10, 10))
+    path.relativeMoveToPoint([0, 3.5])
     
-    path.moveToPoint([100, 100])
-    @repo.commits(:master, 50, 0).each_with_index do |commit, index|
-      point = NSMakePoint(((index + 1) * 25), 100)
-      path.lineToPoint([25 * index, 100])
-      path.appendBezierPathWithOvalInRect(NSMakeRect(point[0] - 10, point[1], 10, 10))
-    end
+    context.saveGraphicsState
+    path.relativeLineToPoint([90, 0])
+    context.restoreGraphicsState
+    
+    path.relativeMoveToPoint([5, 5])
+    path.relativeCurveToPoint_controlPoint1_controlPoint2([40, 20], [0, 20], [-5, 20])
+    
+    path.appendBezierPathWithOvalInRect(NSMakeRect(200, 100, 10, 10))
+    path.relativeMoveToPoint([0, 3.5])
+    path.relativeLineToPoint([90, 0])
+    path.appendBezierPathWithOvalInRect(NSMakeRect(300, 100, 10, 10))
+    
+    
+    
+    # commits = @repo.git.rev_list({:topo_order => true, :all => true, :pretty => 'raw', :full_history => true})
+    # commits = Grit::Commit.list_from_string(@repo, commits)
+    # commits.each_with_index do |commit, index|
+    #   current_offset = 25 * (index + path_offset)
+    #   path.moveToPoint([current_offset - ((path_offset * 25) + 40), 105])
+    #   path.lineToPoint([current_offset, 105])
+    #   path.appendBezierPathWithOvalInRect(NSMakeRect(current_offset, 100, 10, 10))
+    #   commit.parents.each do |parent|
+    #     npath = NSBezierPath.bezierPathWithOvalInRect(NSMakeRect((current_offset + 25), 125, 10, 10))
+    #     NSColor.blueColor.set
+    #     npath.fill
+    #     npath.lineWidth = 3
+    #     NSColor.whiteColor.set
+    #     npath.stroke
+    #     path_offset += 1
+    #   end if commit.parents.size > 1
+    # end
     
     NSColor.blackColor.set
     path.fill
